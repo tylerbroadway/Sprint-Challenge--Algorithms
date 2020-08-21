5496cb8cda29fdc29a8bdd4c9f0e3bc65965a2f1
tyler broadway <tbroadway96@gmail.com>
2020-08-21 11:02:55 -0500

    Completed robot sort using bubble sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,8 +97,10 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
-
+        for i in range(len(self._list) - 1):
+            for j in range(0, len(self._list) - i - 1):
+                if self._list[j] > self._list[j + 1]:
+                    self._list[j], self._list[j + 1] = self._list[j + 1], self._list[j]
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
